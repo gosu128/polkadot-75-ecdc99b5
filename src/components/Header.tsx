32fa4369bf7fbc3dec1c10db6d9a2de4a135a7aa
jasharseyfi <jashar@fftb.co>
2025-03-07 12:16:15 -0600

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,19 +12,22 @@ const Header = () => {
         </a>
 
         {/* Navigation Bar Below Title */}
-        <nav className="mt-3 flex space-x-6">
-          <a href="/methodology" className="text-gray-700 hover:text-polkadot-pink font-medium transition">
-            Methodology
-          </a>
-          <a href="/pmf-scores" className="text-gray-700 hover:text-polkadot-pink font-medium transition">
-            PMF-Scores
-          </a>
-          <a href="/whitepaper" className="text-gray-700 hover:text-polkadot-pink font-medium transition">
-            Whitepaper
-          </a>
-          <a href="/project-background" className="text-gray-700 hover:text-polkadot-pink font-medium transition">
-            Project Background
-          </a>
+        <nav className="mt-4 flex space-x-4">
+          {[
+            { name: "Methodology", link: "/methodology" },
+            { name: "PMF-Scores", link: "/pmf-scores" },
+            { name: "Whitepaper", link: "/whitepaper" },
+            { name: "Project Background", link: "/project-background" }
+          ].map((item, index) => (
+            <a
+              key={index}
+              href={item.link}
+              className="relative px-5 py-2 rounded-full text-gray-700 bg-gray-100 font-medium transition-all duration-300 hover:bg-polkadot-pink hover:text-white"
+            >
+              {item.name}
+              <span className="absolute left-0 bottom-0 w-full h-0.5 bg-polkadot-pink scale-x-0 transition-transform duration-300 ease-out group-hover:scale-x-100"></span>
+            </a>
+          ))}
         </nav>
 
       </div>
@@ -33,3 +36,4 @@ const Header = () => {
 };
 
 export default Header;
+

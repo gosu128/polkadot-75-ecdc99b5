ba19f8b9406eec5e906c4fa216fa85e44c54f720
jasharseyfi <jashar@fftb.co>
2025-03-11 11:38:58 -0600

    Update Methodology.tsx

--- src/pages/Methodology.tsx
+++ src/pages/Methodology.tsx
@@ -7,7 +7,7 @@ const Methodology = () => {
       <Header />
       <div className="container mx-auto p-4 pt-32">
         <iframe 
-          src="https://docs.google.com/presentation/d/e/2PACX-1vTK6cOXHFgXjxuBZbisQTnw26UM6o3ZXV0_6mLJH-lz8_E21gltwrZayHGQ-nS79E1Aa0sSVCsLY0r_/embed?start=false&loop=false&delayms=3000" 
+          src="https://docs.google.com/presentation/d/1_ZD9gZb7flXCKxO_3t4R0HIm1TdjCHltD1hvtU_2z0M/edit?usp=sharing/embed?start=false&loop=false&delayms=3000" 
           className="w-full h-[calc(100vh-200px)]"
           frameBorder="0" 
           allowFullScreen

34dfed48fdf97f285dc4049983388c54e6b0a130
jasharseyfi <jashar@fftb.co>
2025-03-11 12:53:39 -0600

    Update BDTeams.tsx

--- src/pages/BDTeams.tsx
+++ src/pages/BDTeams.tsx
@@ -5,12 +5,14 @@ const BDTeams = () => {
     <div className="w-full min-h-screen bg-white">
       <Header />
       <div className="container mx-auto p-4 pt-32 flex justify-center">
-        <iframe 
-          src="https://docs.google.com/presentation/d/1z13q5HFfK39eZVtA6sdHk8jC-EmTQdnBnIjsW0Acyfk/embed?start=false&loop=false&delayms=3000" 
-          className="w-[90vw] h-[50vw] max-w-6xl max-h-[80vh]" 
-          frameBorder="0" 
-          allowFullScreen
-        />
+        <div className="relative w-full max-w-screen-lg overflow-hidden">
+          <iframe 
+            src="https://docs.google.com/presentation/d/1z13q5HFfK39eZVtA6sdHk8jC-EmTQdnBnIjsW0Acyfk/embed?start=false&loop=false&delayms=3000"
+            className="w-full aspect-video"
+            frameBorder="0"
+            allowFullScreen
+          />
+        </div>
       </div>
     </div>
   );

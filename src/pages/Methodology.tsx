c58af77c1771d831660c69dd35d963f24027faf7
jasharseyfi <jashar@fftb.co>
2025-03-11 12:02:56 -0600

    Update Methodology.tsx

--- src/pages/Methodology.tsx
+++ src/pages/Methodology.tsx
@@ -7,7 +7,7 @@ const Methodology = () => {
       <Header />
       <div className="container mx-auto p-4 pt-32">
         <iframe 
-          src="https://polkadot.com/papers/Polkadot-whitepaper.pdf/embed?start=false&loop=false&delayms=3000" 
+          src="https://qhxgyizmewdtvwebpmie.supabase.co/storage/v1/object/public/docs//methodology.pdf/embed?start=false&loop=false&delayms=3000" 
           className="w-full h-[calc(100vh-200px)]"
           frameBorder="0" 
           allowFullScreen

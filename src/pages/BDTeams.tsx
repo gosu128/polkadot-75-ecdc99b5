516da8061f0703e419cfcfb111e182b7d01702f8
jasharseyfi <jashar@fftb.co>
2025-03-11 13:24:17 -0600

    Update BDTeams.tsx

--- src/pages/BDTeams.tsx
+++ src/pages/BDTeams.tsx
@@ -8,7 +8,7 @@ const BDTeams = () => {
         <div className="bg-white p-2 w-full max-w-[1000px]"> {/* Adjust max width if needed */}
           <iframe 
             src="https://docs.google.com/presentation/d/1z13q5HFfK39eZVtA6sdHk8jC-EmTQdnBnIjsW0Acyfk/embed?start=false&loop=false&delayms=3000" 
-            className="w-full h-[calc(100vw*0.555)] border-0"  
+            className="w-full h-[calc(100vw*0.545)] border-0"  
             frameBorder="0" 
             allowFullScreen
           />

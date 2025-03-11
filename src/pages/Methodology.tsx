de272d0e4de64d397608757383702ceabdcf1638
jasharseyfi <jashar@fftb.co>
2025-03-11 13:53:10 -0600

    Update Methodology.tsx

--- src/pages/Methodology.tsx
+++ src/pages/Methodology.tsx
@@ -42,7 +42,7 @@ const Methodology = () => {
 
           <button 
             onClick={nextSlide} 
-            className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-black/30 text-white p-2 rounded-md hover:bg-black/50 transition border border-white/30"
+            className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-black/30 text-white p-1 rounded-sm hover:bg-black/20 transition border border-white/30"
           >
             ▶
           </button>

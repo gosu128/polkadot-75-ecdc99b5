6445b40b32f8c65c9ed7a44f7f471a178fca5647
jasharseyfi <jashar@fftb.co>
2025-03-11 13:13:42 -0600

    Update BDTeams.tsx

--- src/pages/BDTeams.tsx
+++ src/pages/BDTeams.tsx
@@ -2,17 +2,21 @@ import Header from '@/components/Header';
 
 const BDTeams = () => {
   return (
-    <div className="bg-white p-2">
-  <iframe 
-    src="https://docs.google.com/presentation/d/1z13q5HFfK39eZVtA6sdHk8jC-EmTQdnBnIjsW0Acyfk/embed?start=false&loop=false&delayms=3000" 
-    className="w-full h-[calc(100vh-240px)] border-0"
-    frameBorder="0" 
-    allowFullScreen
-  />
-</div>
+    <div className="w-full min-h-screen bg-white">
+      <Header />
+      <div className="container mx-auto p-4 pt-32">
+        <div className="bg-white p-2">
+          <iframe 
+            src="https://docs.google.com/presentation/d/1z13q5HFfK39eZVtA6sdHk8jC-EmTQdnBnIjsW0Acyfk/embed?start=false&loop=false&delayms=3000" 
+            className="w-full h-[calc(100vh-240px)] border-0"
+            frameBorder="0" 
+            allowFullScreen
+          />
+        </div>
       </div>
     </div>
   );
 };
 
 export default BDTeams;
+

e97f309ae19c45eeaaec6df62b58c6bb7ee16b1b
jasharseyfi <jashar@fftb.co>
2025-03-11 13:11:38 -0600

    Update BDTeams.tsx

--- src/pages/BDTeams.tsx
+++ src/pages/BDTeams.tsx
@@ -2,17 +2,14 @@ import Header from '@/components/Header';
 
 const BDTeams = () => {
   return (
-    <div className="w-full min-h-screen bg-white">
-      <Header />
-      <div className="container mx-auto p-4 pt-32 flex justify-center">
-        <div className="relative w-full max-w-screen-lg overflow-hidden">
-         <iframe 
-  src="https://docs.google.com/presentation/d/1z13q5HFfK39eZVtA6sdHk8jC-EmTQdnBnIjsW0Acyfk/embed?start=false&loop=false&delayms=3000" 
-  className="w-full h-[calc(100vh-240px)] border-0 bg-white" /* Removed border */
-  frameBorder="0" 
-  allowFullScreen
-/>
-        </div>
+    <div className="bg-white p-2">
+  <iframe 
+    src="https://docs.google.com/presentation/d/1z13q5HFfK39eZVtA6sdHk8jC-EmTQdnBnIjsW0Acyfk/embed?start=false&loop=false&delayms=3000" 
+    className="w-full h-[calc(100vh-240px)] border-0"
+    frameBorder="0" 
+    allowFullScreen
+  />
+</div>
       </div>
     </div>
   );

499dcd62838b6ad9553c83ecae5c29e2d7dbfdd3
jasharseyfi <jashar@fftb.co>
2025-03-11 13:16:43 -0600

    Update BDTeams.tsx

--- src/pages/BDTeams.tsx
+++ src/pages/BDTeams.tsx
@@ -5,10 +5,10 @@ const BDTeams = () => {
     <div className="w-full min-h-screen bg-white">
       <Header />
       <div className="container mx-auto p-4 pt-32 flex justify-center">
-        <div className="bg-white p-2 w-full max-w-[1280px]"> {/* Adjust max width */}
+        <div className="bg-white p-2 w-full max-w-[1150px]"> {/* Reduced width from 1280px to 1150px */}
           <iframe 
             src="https://docs.google.com/presentation/d/1z13q5HFfK39eZVtA6sdHk8jC-EmTQdnBnIjsW0Acyfk/embed?start=false&loop=false&delayms=3000" 
-            className="w-full aspect-[16/9] border-0"  // Enforce 16:9 aspect ratio
+            className="w-full aspect-[16/9] border-0"  
             frameBorder="0" 
             allowFullScreen
           />
@@ -19,3 +19,4 @@ const BDTeams = () => {
 };
 
 export default BDTeams;
+

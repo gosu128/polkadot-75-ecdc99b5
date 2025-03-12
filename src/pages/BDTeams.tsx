3aea0c27e0eb1dbb160edce0547c2b2832252bbc
jasharseyfi <jashar@fftb.co>
2025-03-12 05:20:17 -0600

    Update BDTeams.tsx

--- src/pages/BDTeams.tsx
+++ src/pages/BDTeams.tsx
@@ -7,11 +7,10 @@ const BDTeams = () => {
       <div className="container mx-auto p-4 pt-32 flex justify-center">
         <div className="relative w-full max-w-[1100px]">
           {/* Aspect Ratio Wrapper */}
-          <div className="relative w-full" style={{ paddingTop: "56.25%" }}>
+          <div className="relative w-full overflow-hidden" style={{ paddingTop: "56.25%" }}>
             <iframe
               src="https://docs.google.com/presentation/d/1z13q5HFfK39eZVtA6sdHk8jC-EmTQdnBnIjsW0Acyfk/embed?start=false&loop=false&delayms=3000"
-              className="absolute top-0 left-0 w-full h-full border-0 scale-[1.12] origin-top"
-              style={{ transform: "scale(1.12) translateY(4%)" }} // Adjusted positioning
+              className="absolute top-0 left-0 w-full h-full border-0 scale-[1.10] origin-center"
               frameBorder="0"
               allowFullScreen
             />
@@ -23,4 +22,3 @@ const BDTeams = () => {
 };
 
 export default BDTeams;
-

e5610680e77084c233734790bc53d005e3445524
jasharseyfi <jashar@fftb.co>
2025-03-12 05:28:17 -0600

    Update BDTeams.tsx

--- src/pages/BDTeams.tsx
+++ src/pages/BDTeams.tsx
@@ -7,7 +7,7 @@ const BDTeams = () => {
       <div className="container mx-auto p-4 pt-32 flex justify-center">
         <div className="relative w-full max-w-[900px]"> {/* Made the slide deck smaller */}
           {/* 4:3 Aspect Ratio Wrapper */}
-          <div className="relative w-full" style={{ paddingTop: "66.67%" }}>  
+          <div className="relative w-full" style={{ paddingTop: "62.5%" }}>  
             <iframe
               src="https://docs.google.com/presentation/d/1z13q5HFfK39eZVtA6sdHk8jC-EmTQdnBnIjsW0Acyfk/embed?start=false&loop=false&delayms=3000"
               className="absolute top-0 left-0 w-full h-full border-0"

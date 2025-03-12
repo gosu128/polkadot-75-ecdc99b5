f5696dc0a62105add0bcf3171dd7c0818f7bb3c4
jasharseyfi <jashar@fftb.co>
2025-03-12 05:17:43 -0600

    Update BDTeams.tsx

--- src/pages/BDTeams.tsx
+++ src/pages/BDTeams.tsx
@@ -10,7 +10,7 @@ const BDTeams = () => {
           <div className="relative w-full overflow-hidden" style={{ paddingTop: "56.25%" }}>
             <iframe
               src="https://docs.google.com/presentation/d/1z13q5HFfK39eZVtA6sdHk8jC-EmTQdnBnIjsW0Acyfk/embed?start=false&loop=false&delayms=3000"
-              className="absolute top-0 left-0 w-full h-full border-0 scale-[1.2] origin-center"
+              className="absolute top-0 left-0 w-full h-full border-0 scale-[1.12] origin-center"
               frameBorder="0"
               allowFullScreen
             />

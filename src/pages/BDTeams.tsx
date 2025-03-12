5d397cc9a9434dd4ab9bdeec4c0e13b0fc9d87bb
jasharseyfi <jashar@fftb.co>
2025-03-12 05:32:04 -0600

    Update BDTeams.tsx

--- src/pages/BDTeams.tsx
+++ src/pages/BDTeams.tsx
@@ -1,13 +1,13 @@
-import Header from '@/components/Header';
+mport Header from '@/components/Header';
 
 const BDTeams = () => {
   return (
     <div className="w-full min-h-screen bg-white">
       <Header />
       <div className="container mx-auto p-4 pt-32 flex justify-center">
-        <div className="relative w-full max-w-[900px] p-4 bg-white border-8 border-white shadow-lg rounded-lg">  
-          {/* 4:3 Aspect Ratio Wrapper (or adjust as needed) */}
-          <div className="relative w-full" style={{ paddingTop: "60.3%" }}>  
+        <div className="relative w-full max-w-[900px]"> {/* Made the slide deck smaller */}
+          {/* 4:3 Aspect Ratio Wrapper */}
+          <div className="relative w-full" style={{ paddingTop: "60.2%" }}>  
             <iframe
               src="https://docs.google.com/presentation/d/1z13q5HFfK39eZVtA6sdHk8jC-EmTQdnBnIjsW0Acyfk/embed?start=false&loop=false&delayms=3000"
               className="absolute top-0 left-0 w-full h-full border-0"

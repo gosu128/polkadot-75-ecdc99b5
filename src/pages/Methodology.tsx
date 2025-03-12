f6103b44e5078088e911e20e31f3e54c55724c74
jasharseyfi <jashar@fftb.co>
2025-03-12 05:43:13 -0600

    Update Methodology.tsx

--- src/pages/Methodology.tsx
+++ src/pages/Methodology.tsx
@@ -10,7 +10,7 @@ const Methodology = () => {
           {/* 4:3 Aspect Ratio Wrapper */}
           <div className="relative w-full" style={{ paddingTop: "60.3%" }}>  
             <iframe
-              src="https://docs.google.com/presentation/d/1_ZD9gZb7flXCKxO_3t4R0HIm1TdjCHltD1hvtU_2z0M/edit?usp=sharing/embed?start=false&loop=false&delayms=3000"
+              src="https://docs.google.com/presentation/d/1_ZD9gZb7flXCKxO_3t4R0HIm1TdjCHltD1hvtU_2z0M/present/embed?start=false&loop=false&delayms=3000"
               className="absolute top-0 left-0 w-full h-full border-0"
               frameBorder="0"
               allowFullScreen

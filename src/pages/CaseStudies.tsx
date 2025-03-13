eff7f8e5545dd3771214a860e6803a7b64c09931
jasharseyfi <jashar@fftb.co>
2025-03-13 10:50:23 -0600

    Update CaseStudies.tsx

--- src/pages/CaseStudies.tsx
+++ src/pages/CaseStudies.tsx
@@ -22,11 +22,11 @@ const CaseStudies = () => {
         <div className="relative w-full max-w-[900px]">
           <div className="relative w-full" style={{ paddingTop: "60.3%" }}>  
             <iframe 
-              src="https://docs.google.com/presentation/d/1n5gn_0_LyxJKXHN8X9_K2euMnyD9ecF67Pta9OVF39A/present" 
-              className="absolute top-0 left-0 w-full h-full border-0" 
-              frameBorder="0" 
-              allowFullScreen 
-            />
+  src="https://docs.google.com/presentation/d/1n5gn_0_LyxJKXHN8X9_K2euMnyD9ecF67Pta9OVF39A/embed?start=false&loop=false&delayms=3000" 
+  className="absolute top-0 left-0 w-full h-full border-0" 
+  frameBorder="0" 
+  allowFullScreen 
+/>
           </div>
         </div>
 

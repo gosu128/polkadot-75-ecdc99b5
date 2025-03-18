a408e52db3bae1afe26dd30ed7bb61615c7168a0
jasharseyfi <jashar@fftb.co>
2025-03-18 10:46:25 -0600

    Update Resources.tsx

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -28,11 +28,11 @@ const Resources = () => {
           <h2 className="text-2xl font-bold text-polkadot-pink mb-8 text-center">Proof Points</h2>
           <hr className="border-t-2 border-gray-300 mb-10" />
           
-          {/* Google Sheets Embedded Document */}
+          {/* Google Sheets in Presentation Mode */}
           <div className="relative w-full max-w-[900px] mx-auto">
             <div className="relative w-full" style={{ paddingTop: "60.3%" }}>  
               <iframe 
-                src="https://docs.google.com/spreadsheets/d/1rzMvSxH5IBAzb8EzezO-1dYneX6nIkDUIs-7MhFirgw/preview?gid=842874053#gid=842874053&range=A1"
+                src="https://docs.google.com/spreadsheets/d/1rzMvSxH5IBAzb8EzezO-1dYneX6nIkDUIs-7MhFirgw/preview?gid=842874053"
                 className="absolute top-0 left-0 w-full h-full border-0" 
                 frameBorder="0" 
                 allowFullScreen 
@@ -41,14 +41,14 @@ const Resources = () => {
           </div>
         </section>
 
-        {/* Case Studies (Third - New Position) */}
+        {/* Case Studies (Third - New Document) */}
         <section id="case-studies" className="w-full">
           <h2 className="text-2xl font-bold text-polkadot-pink mb-8 text-center">Case Studies</h2>
           <hr className="border-t-2 border-gray-300 mb-10" />
           <div className="relative w-full max-w-[900px] mx-auto">
             <div className="relative w-full" style={{ paddingTop: "60.3%" }}>  
               <iframe 
-                src="https://docs.google.com/presentation/d/1JhftKc5G9VVjvD59tOk06SzMxB5FQclGu4P1i-tQ_gg/embed?start=false&loop=false&delayms=3000"
+                src="https://docs.google.com/presentation/d/1miW8bJb2ZHLrMjMlQC6IC5r-qDHtXxdoR1mwxsZ7uEg/embed?start=false&loop=false&delayms=3000"
                 className="absolute top-0 left-0 w-full h-full border-0" 
                 frameBorder="0" 
                 allowFullScreen 
@@ -80,3 +80,4 @@ const Resources = () => {
 };
 
 export default Resources;
+
